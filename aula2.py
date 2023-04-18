d4ff24a45b9e466e7d60d9e5c5fc40f87aab9aa0
Bruno Costa <bruno@email.com>
2023-04-17 21:06:01 -0300

    Criado Aula 2 em python

---
+++ aula2.py
@@ -0,0 +1,14 @@
+nome = input("Informe seu nome: ")
+idade = input("Informe sua idade: ")
+
+#Isso é um comentário de uma linha
+"""
+Isso é um cometário de várias
+linhas
+jhsadgfjhsagfdhsja
+ashjgfsjhagdjhsa
+"""
+# Primeira forma de concatenar
+print("Olá ",nome," sua idade é ",idade)
+print("Olá {} sua idade é {}".format(nome,idade))
+print(f"Olá {nome} sua idade é {idade}")
