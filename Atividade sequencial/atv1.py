aa7236bd49435afc1d0cc82786fcb8439dce39bb
Bruno Costa <bruno@email.com>
2023-04-20 19:52:15 -0300

    Finalizado atividade sequencial

---
+++ Atividade sequencial/atv1.py
@@ -0,0 +1,4 @@
+idade = int(input("Informe sua idade: "))
+
+print(f"Sua idade em meses é {idade * 12} meses")
+print(f"Sua idade em dias é {idade * 365} dias")
---
+++ Atividade sequencial/atv3.py
@@ -0,0 +1,6 @@
+nome = input("Informe o nome da fazenda: ")
+totalCavalos = int(input("Informe a quantidade de cavalos: "))
+
+totalFerraduras = totalCavalos * 4
+
+print(f"A fazenda {nome} possui {totalCavalos} cavalos, a quantidade total de ferradura para equipar todos os cavalos é: {totalFerraduras}")
