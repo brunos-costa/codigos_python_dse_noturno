08278294927721a27d8aecadc983b4914699b90b
Bruno Costa <analistabrunocosta@gmail.com>
2023-05-15 21:09:27 -0300

    Finalizado aula dicionário

---
+++ aula11-dicionario.py
@@ -0,0 +1,22 @@
+lista = ["João", 30, "Cohab"]
+pessoa = {
+    "nome":"Maria",
+    "idade":24,
+    "bairro":"Renascença"
+}
+
+print(lista[0])
+print(pessoa,"\n")
+
+# EXIBINDO AS CHAVES UTILIZANDO O COMANDO KEYS()
+print(pessoa.keys())
+
+# EXIBINDO OS VALORES UTILIZANDO O CAMANDO VALUES()
+print(pessoa.values())
+
+# EXIBINDO TANTO A CHAVE COMO VALOR UTILIZANDO O CAMANDO ITEMS()
+print(pessoa.items())
+
+# EXIBINDO O DICIONÁRIO MAIS DETALHADO
+for chave, valor in pessoa.items():
+    print(f"{chave}: {valor}")
---
+++ aula11.1-dicionario-cont.py
@@ -0,0 +1,29 @@
+import os
+estado = {"uf": "Maranhão", "sigla":"MA"}
+
+os.system("cls")
+print(estado)
+
+# INSERINDO DADOS EM UM DICIONÁRIO
+# FORMA 1
+estado["populacao"] = "20.000.000"
+print(estado)
+
+#FORMA 2
+estado.update({"capital":"São Luis"})
+print(estado)
+
+#REMOVENDO ITENS DO DICIONÁRIO
+#FORMA 1
+estado.pop("uf")
+print(estado)
+
+#FORMA 2
+del(estado["populacao"])
+print(estado)
+
+# FORMA 3 - APAGAR TODO CONTEÚDO
+estado.clear()
+print(estado)
+
+
