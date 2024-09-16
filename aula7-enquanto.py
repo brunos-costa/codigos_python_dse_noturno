449642cfe603aa4a54b661de79da3b8deee5f0ce
Bruno Costa <analistabrunocosta@gmail.com>
2024-09-16 09:11:21 -0300

    Melhorado exemplo while

--- aula7-enquanto.py
+++ aula7-enquanto.py
@@ -1,27 +1,28 @@
-# While funcionando como loop contado 
-contador = 1
+#1ª forma de utilizar while - parecido com o FOR
+contador = 1 # inicializando a variável
 
 while contador <= 5:
-    print(contador) 
-    contador = contador + 1
+    print(contador)
+    contador = contador + 1 # é o mesmo que contador +=1
 
-# While funcionando como loop condicional
-"""
-senha = ""
-while senha != "123":
-    senha = input("Informe sua senha: ")
 
-print("Obrigado por usar o sistema\n")
-"""
-#While como se fosse o faça - enquanto
+print("="*40)
+
+# 2ª forma de utilizar while - loop condicional normal
+
+valor = 0 # inicilizando a variável
+while valor >=0:
+    valor = int(input("Informe um valor qualquer(digite um valor negativo para terminar): "))
+    
+    print(f"Você digitou {valor}")
+
+print("="*40)
+
+#3ª forma de utilizar while - como se fosse faça..enquanto
 while True:
-    senha = input("Informe sua senha: ")
+    senha = input("Informe a senha: ")
     if senha == "123":
-        break
-
-print("Obrigado")
-
- 
-
-
-
+        print("Parabéns, Senha correta")
+        break #forma de encerrar o while
+    else:
+        print("Senha incorreta, tente denovo")
